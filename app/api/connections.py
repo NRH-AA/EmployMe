6d03826d8c6ef34deea6832b6db5003238349e23
NRH-AA <nheinz1995@live.com>
2023-04-20 11:52:38 -0700

    Add date to posts

--- app/api/connections.py
+++ app/api/connections.py
@@ -12,9 +12,6 @@ def manage_connection():
     connecter = int(data['connecter'])
     connecting = int(data['connecting'])
     
-    print(connecter)
-    print(connecting)
-    
     user = User.query.get(connecter)
     if not user:
         return {'errors': ['Error getting user']}, 400
@@ -25,10 +22,12 @@ def manage_connection():
     
     if other_user in user.connection:
         user.connection.remove(other_user)
+        user.following.remove(other_user)
         db.session.commit()
         return {'message': 'success'}
     
     user.connection.append(other_user)
+    user.following.append(other_user)
     db.session.commit()
     
     return {'message': 'success'}
