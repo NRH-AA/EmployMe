50036ab2be79f050be7c962c82387fbe2460f363
NRH-AA <nheinz1995@live.com>
2023-04-22 09:43:14 -0700

    Add Post Comments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from .db import db
 from .user import User
 from .company import Company
 from .post import Post
+from .comment import Comment
 from .message import Message
 from .user_image import UserImage
 from .post_image import PostImage
